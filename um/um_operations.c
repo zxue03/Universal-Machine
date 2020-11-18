e9084f75dd108e1f54d7cab05dee45e84ce60932
Amend3d <ironicplayer6@gmail.com>
2020-11-17 21:00:24 -0500

    Creation of um_operations.c

--- um/um_operations.c
+++ um/um_operations.c
@@ -1,1 +1,8 @@
+/*
+*   um_operations.c
+*   Authors: Louis Xue (zxue03) and Kevin Gao (kgao03)
+*
+*   This class implements the functions of um_operations, which handle the
+*   execution of each operation instruction
+*/
 #include "um_operations.h"
