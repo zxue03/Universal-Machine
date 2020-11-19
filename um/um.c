9273fec8220a0ab3c836e17db2b5815f75a64d12
Amend3d <ironicplayer6gmail.com>
2020-11-18 21:33:53 -0500

    Implementation of driver function

--- um/um.c
+++ um/um.c
@@ -1,9 +1,26 @@
-#include "um_engine.h"
-#include "um_operations.h"
+/*
+*   um.c
+*   Authors: Louis Xue (zxue03) and Kevin Gao (kgao03)
+*
+*   This file holds the driver function for the program, opening the um
+*   instruction file and starting an emulator
+*/
 
+#include "um_engine.h"
+#include <assert.h>
 int main(int argc, char **argv)
 {
-    (void)argc;
-    (void)argv;
-    printf("Hello UM\n");
+    // Open the file
+    if (argc != 2) {
+        fprintf(stderr, "Usage: ./um [um instruction file]");
+        exit(EXIT_FAILURE);
+    }
+    FILE *fp = fopen(argv[1]);
+    if (fp == NULL) {
+        fprintf(stderr, "Specified um instruction file does not exist.");
+        exit(EXIT_FAILURE);
+    }
+
+    // Create and run a UM emulator
+    run_um(fp);
 }
