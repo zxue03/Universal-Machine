364b1fbaf279fd6835b92ff58e4c8086d03cae2a
Amend3d <ironicplayer6@gmail.com>
2020-11-22 20:53:10 -0500

    Automatic diff success check

--- um/run_tests.sh
+++ um/run_tests.sh
@@ -15,6 +15,7 @@ mv *.1 tests
 cd tests
 testFiles=$(ls *.um)
 echo ""
+allSucceed=1
 echo "==========STARTING TESTS=========="
 for testFile in $testFiles ; do
     testName=$(echo $testFile | sed -E 's/(.*).um/\1/')
@@ -27,12 +28,40 @@ for testFile in $testFiles ; do
         ../um "$testName.um" > "$testName.mine"
     fi
     diff "$testName.rf" "$testName.mine"
+    code=$?
+    if [ ! $code -eq 0 ] ; then
+        echo ""
+        echo "********ERROR********"
+        echo "Diffing $testName.rf and $testName.mine failed."
+        echo ""
+        allSucceed=0
+    fi
     if [ -e "$testName.1" ] ; then
         diff "$testName.rf" "$testName.1"
+        code=$?
+        if [ ! $code -eq 0 ] ; then
+            echo ""
+            echo "********ERROR********"
+            echo "Diffing $testName.rf and $testName.1 failed."
+            echo ""
+            allSucceed=0
+        fi
         diff "$testName.mine" "$testName.1"
+        code=$?
+        if [ ! $code -eq 0 ] ; then
+            echo ""
+            echo "********ERROR********"
+            echo "Diffing $testName.1 and $testName.mine failed."
+            echo ""
+            allSucceed=0
+        fi
     fi
     echo " Done!"
 done
 echo "==========TESTING FINISHED=========="
 echo ""
-echo "If no diff results showed, all tests succeeded!"
+if [ $allSucceed -eq 1 ] ; then
+    echo "All Tests Succeeded!"
+else
+    echo "Some Tests Failed."
+fi
