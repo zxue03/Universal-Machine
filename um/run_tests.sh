422aff9c5ab1d428b4343017c273a07c44847f65
Amend3d <ironicplayer6@gmail.com>
2020-11-22 19:52:33 -0500

    Fully functional testing shell script

--- um/run_tests.sh
+++ um/run_tests.sh
@@ -1,9 +1,29 @@
 #! /bin/sh
 make all
 ./writetests
+cd tests
+rm *.um
+rm *.0
+rm *.1
+cd ../
 mv *.um tests
 mv *.0 tests
 mv *.1 tests
 cd tests
 testFiles=$(ls *.um)
-echo $testFiles
+for testFile in $testFiles ; do
+    testName=$(echo $testFile | sed -E 's/(.*).um/\1/')
+    if [ -e "$testName.0" ] ; then
+        um "$testName.um" < "$testName.0" > "$testName.rf"
+        ../um "$testName.um" < "$testName.0" > "$testName.mine"
+    else
+        um "$testName.um" > "$testName.rf"
+        ../um "$testName.um" > "$testName.mine"
+    fi
+    diff "$testName.rf" "$testName.mine"
+    if [ -e "$testName.1" ] ; then
+        diff "$testName.rf" "$testName.1"
+        diff "$testName.mine" "$testName.1"
+    fi
+done
+echo "If no diff results showed, all tests succeeded."
